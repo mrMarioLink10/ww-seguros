c7df1b7d87146116a3e2fce4adf5a2e192d1a78f
Isai Vargas <alam@C02LWRC6F8J2.local>
2020-03-06 13:57:51 -0400

    feature: maquetacion de tipo de reclamacion reclamacion

--- src/app/modules/dashboard/claims/new-claim/claim-types/claim/claim.component.ts
+++ src/app/modules/dashboard/claims/new-claim/claim-types/claim/claim.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-claim',
-  templateUrl: './claim.component.html',
-  styleUrls: ['./claim.component.scss']
+	selector: 'app-claim',
+	templateUrl: './claim.component.html',
+	styleUrls: [ './claim.component.scss' ]
 })
 export class ClaimComponent implements OnInit {
+	step = 0;
+	Date: any;
+	initialDate: any;
+	finalDate: any;
+	constructor() {}
 
-  constructor() { }
+	ngOnInit() {}
 
-  ngOnInit() {
-  }
+	setStep(index: number) {
+		this.step = index;
+	}
 
+	nextStep() {
+		this.step++;
+	}
 }

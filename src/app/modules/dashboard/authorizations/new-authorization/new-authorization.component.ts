d9ee3965ddf0711ba664993001c03578ef6becda
zer0 <rhailinmade@hotmail.com>
2020-03-20 09:58:03 -0400

    autorizaciones

--- src/app/modules/dashboard/authorizations/new-authorization/new-authorization.component.ts
+++ src/app/modules/dashboard/authorizations/new-authorization/new-authorization.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FieldConfig } from '../../../../shared/components/form-components/models/field-config'
+
 
 @Component({
   selector: 'app-new-authorization',
@@ -6,6 +8,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-authorization.component.scss']
 })
 export class NewAuthorizationComponent implements OnInit {
+
+  
+  accordionTitles = [
+		'Información del Asegurado',
+		'Información Médica'
+  ];
+
+  generos: FieldConfig = {
+		label: 'Sexo',
+		options: [
+			{
+				value: 'femenino',
+				viewValue: 'Femenino'
+			},
+			{
+				value: 'masculino',
+				viewValue: 'Masculino'
+			}
+		]
+  };
+  
+  seguros: FieldConfig = {
+		label: '¿Tiene otro seguro de salud?',
+		options: [
+			{
+				value: 'si',
+				viewValue: 'Si'
+			},
+			{
+				value: 'no',
+				viewValue: 'No'
+			}
+		]
+  };
+  
+  condiciones: FieldConfig = {
+		label: 'La condición se trata de:',
+		options: [
+			{
+				value: 'hospitalizacion',
+				viewValue: 'Hospitalización'
+			},
+			{
+				value: 'cirugia_electiva',
+				viewValue: 'Cirugía Electiva'
+      },
+      {
+				value: 'ambulatorio',
+				viewValue: 'Ambulatorio'
+      },
+      {
+				value: 'estudios_especiales',
+				viewValue: 'Estudios Especiales'
+			}
+		]
+	};
 
   constructor() { }
 

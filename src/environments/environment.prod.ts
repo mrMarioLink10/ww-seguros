921b8964c353170d26eaf1414b61a5daa3696e44
Isai Vargas <isaidvargas@hotmail.com>
2020-04-02 18:32:44 -0400

    fix: env prod base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'https://wwsdevportalbackend.azurewebsites.net'
+
 };
 
-export const baseUrl = 'https://wwsdevportalbackend.azurewebsites.net';

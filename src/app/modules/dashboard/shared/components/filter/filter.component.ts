a69c8fb9090ab7fd34f0a3494dbd670cec2ba1cd
Isai Vargas <isaidvargas@hotmail.com>
2020-04-17 15:55:56 -0400

    hotfix

--- src/app/modules/dashboard/shared/components/filter/filter.component.ts
+++ src/app/modules/dashboard/shared/components/filter/filter.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input,Output,  EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MatFormField } from '@angular/material';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { HttpParams } from '@angular/common/http';
@@ -6,43 +6,43 @@ import { HttpParams } from '@angular/common/http';
 @Component({
 	selector: 'app-filter',
 	templateUrl: './filter.component.html',
-	styleUrls: [ './filter.component.scss' ]
+	styleUrls: ['./filter.component.scss']
 })
 export class FilterComponent implements OnInit {
 	@Input() fills: fills;
 	@Output() public childEvent = new EventEmitter();
-	private filterForm: FormGroup;
+	filterForm: FormGroup;
 	test = true;
 
 	constructor(private _fb: FormBuilder) {
 	}
 
-	sendFilterParamsToParent(filterParams: FormData){
+	sendFilterParamsToParent(filterParams: FormData) {
 		let from = this.setDateFormatToYYYMMDD(this.filterForm.get('from').value);
 		let to = this.setDateFormatToYYYMMDD(this.filterForm.get('to').value);
 
 		let params = new HttpParams();
 		params = params.append('name', this.filterForm.get('name').value)
 		params = params.append('status', this.filterForm.get('status').value);
-		
-		if(this.fills.fillType == 'tipoSeguro') {
+
+		if (this.fills.fillType == 'tipoSeguro') {
 			params = params.append('tipoSeguro', this.filterForm.get('tipoSeguro').value);
 		}
 		else {
 			params = params.append('nroPoliza', this.filterForm.get('nroPoliza').value);
 		}
 
-		if(from) params = params.append('from', from);
-		if(to) params = params.append('to', to);
+		if (from) params = params.append('from', from);
+		if (to) params = params.append('to', to);
 
 		this.childEvent.emit(params);
 	}
 
-	setDateFormatToYYYMMDD(datePickerValue){
+	setDateFormatToYYYMMDD(datePickerValue) {
 		let date;
-		if(datePickerValue){
+		if (datePickerValue) {
 			let yyyy = datePickerValue.getFullYear();
-			let mm = datePickerValue.getMonth()+1;
+			let mm = datePickerValue.getMonth() + 1;
 			let dd = datePickerValue.getDate();
 			date = (`${yyyy}-${mm}-${dd}`);
 		}
@@ -51,7 +51,7 @@ export class FilterComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		if(this.fills.fillType == 'tipoSeguro'){
+		if (this.fills.fillType == 'tipoSeguro') {
 			this.filterForm = this._fb.group({
 				from: [''],
 				to: [''],
@@ -59,7 +59,7 @@ export class FilterComponent implements OnInit {
 				status: [''],
 				tipoSeguro: ['']
 			});
-		}else {
+		} else {
 			this.filterForm = this._fb.group({
 				from: [''],
 				to: [''],
@@ -72,6 +72,6 @@ export class FilterComponent implements OnInit {
 }
 
 interface fills {
-	statusTypes: [],
+	status: [],
 	fillType: string
 }

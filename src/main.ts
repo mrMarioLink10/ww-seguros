5c5eafccc870b8b9bdd28a2eaa392c639d016d87
Isai Vargas <isaidvargas@hotmail.com>
2020-05-01 16:06:35 -0400

    fix: eliminar iniciacion de bootstrap

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
 
 // keycloak init options
 const initOptions = {

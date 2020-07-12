327635e16bad0d7940807d1624da042090616782
Isai Vargas <isaidvargas@hotmail.com>
2020-07-12 12:17:55 -0400

    fix: arreglar urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { KeycloakConfig } from 'keycloak-angular';
 
 // Add here your keycloak setup infos
 const keycloakConfig: KeycloakConfig = {
-  url: 'https://wwskeycloak.eastus.cloudapp.azure.com:8080/auth',
+  url: 'http://wwskeycloak.eastus.cloudapp.azure.com:8080/auth',
   realm: 'worldwide',
   clientId: 'cotizador',
 };
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ if (environment.production) {
 
 // keycloak init options
 const initOptions = {
-  url: 'https://wwskeycloak.eastus.cloudapp.azure.com:8080/auth', realm: 'worldwide', clientId: 'cotizador'
+  url: 'http://wwskeycloak.eastus.cloudapp.azure.com:8080/auth', realm: 'worldwide', clientId: 'cotizador'
 };
 
 const keycloak = Keycloak(initOptions);

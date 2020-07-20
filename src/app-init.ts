7f01b0542dbb453505b6befd1e851352234fd570
Isai Vargas <isaidvargas@hotmail.com>
2020-07-20 11:47:23 -0400

    fix: arreglo del refresh token en nuevo flujo de keycloak

--- src/app-init.ts
+++ src/app-init.ts
@@ -21,6 +21,26 @@ export function initializer(keycloak: KeycloakService): () => Promise<any> {
                 localStorage.setItem('ang-token', keycloak._instance.token);
                 localStorage.setItem('ang-refresh-token', keycloak._instance.refreshToken);
                 localStorage.setItem('user-information', JSON.stringify(keycloak._instance.tokenParsed));
+
+                // tslint:disable: max-line-length
+                setInterval(() => {
+                    // console.log('No te mentire, se ve muy fresco el pana token, se vence en: ' + Math.round(keycloak._instance.tokenParsed.exp + keycloak._instance.timeSkew - new Date().getTime() / 1000) + ' segundos');
+                    keycloak.updateToken(300).then((refreshed) => {
+                        if (refreshed) {
+                            console.log('No te mentire, se ve muy fresco el pana token, se vence en: '
+                                + Math.round(keycloak._instance.tokenParsed.exp + keycloak._instance.timeSkew - new Date().getTime() / 1000) + ' segundos');
+                            localStorage.setItem('ang-token', keycloak._instance.token);
+
+                        } else {
+                            console.warn('Token no refrescado, token no refrescado, donde estan sus padres? Se vence en: '
+                                + Math.round(keycloak._instance.tokenParsed.exp + keycloak._instance.timeSkew - new Date().getTime() / 1000) + ' segundos');
+                        }
+                    }).catch(() => {
+                        console.error('Failed to refresh token');
+                    });
+
+                }, 180000);
+
             } catch (error) {
                 reject(error);
             }

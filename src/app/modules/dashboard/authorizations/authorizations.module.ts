d8f303dc2458e5312134affe9682f44dc9aabe0a
Isai Vargas <isaidvargas@hotmail.com>
2020-07-20 15:50:49 -0400

    fix: mask de numeros de telefonos

--- src/app/modules/dashboard/authorizations/authorizations.module.ts
+++ src/app/modules/dashboard/authorizations/authorizations.module.ts
@@ -7,6 +7,7 @@ import { NewAuthorizationComponent } from './new-authorization/new-authorization
 import { GlobalSharedModule } from '../../../shared/global-shared.module';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 
 
 @NgModule({
@@ -17,7 +18,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     GlobalSharedModule,
     SharedModule,
     ReactiveFormsModule,
-
+    NgxMaskModule
   ]
 })
 export class AuthorizationsModule { }
--- src/app/shared/global-shared.module.ts
+++ src/app/shared/global-shared.module.ts
@@ -5,10 +5,11 @@ import { MaterialModule } from 'src/app/shared/modules/material.module';
 import { MatDialogModule } from '@angular/material';
 import { FormComponentsModule } from './components/form-components/form-components.module';
 import { FormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
 	declarations: [BaseDialogComponent],
-	imports: [CommonModule, MaterialModule, FormComponentsModule, MatDialogModule, FormsModule],
+	imports: [CommonModule, MaterialModule, FormComponentsModule, MatDialogModule, FormsModule, NgxMaskModule],
 	exports: [FormComponentsModule, MaterialModule],
 	entryComponents: [BaseDialogComponent]
 })

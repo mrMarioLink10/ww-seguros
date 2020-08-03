1dd9b11c7510e55ff9432ede10f43c7983571a60
Isai Vargas <isaidvargas@hotmail.com>
2020-08-03 11:03:26 -0400

    fix: eliminar campo de nombre de tratamiento

--- src/app/modules/dashboard/requests/new-request/life/life.component.ts
+++ src/app/modules/dashboard/requests/new-request/life/life.component.ts
@@ -2842,7 +2842,7 @@ export class LifeComponent implements OnInit, DoCheck {
 
       case 'insurance':
         return this.fb.group({
-          name: ['', Validators.required],
+          // name: ['', Validators.required],
           detail: ['', Validators.required],
         });
         break;

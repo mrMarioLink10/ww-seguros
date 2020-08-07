6299c1f47106b7216eb1b1253d17a018d1e409e2
Isai Vargas <isaidvargas@hotmail.com>
2020-08-07 12:18:24 -0400

    feature: creacion ts para ailments

---
+++ src/app/core/class/major-expenses-ailments.ts
@@ -0,0 +1,607 @@
+import { FieldConfig } from 'src/app/shared/components/form-components/models/field-config';
+
+export const $musculoSkeletalAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'ARTRITIS',
+            viewValue: 'ARTRITIS',
+        },
+        {
+            value: 'NEURITIS',
+            viewValue: 'NEURITIS',
+        },
+        {
+            value: 'REUMATISMO',
+            viewValue: 'REUMATISMO',
+        },
+        {
+            value: 'OSTEOPOROSIS',
+            viewValue: 'OSTEOPOROSIS',
+        },
+        {
+            value: 'LUMBAGO',
+            viewValue: 'LUMBAGO',
+        },
+        {
+            value: 'HERNIA DISCAL',
+            viewValue: 'HERNIA DISCAL',
+        },
+        {
+            value: 'ESCOLIOSIS',
+            viewValue: 'ESCOLIOSIS',
+        },
+        {
+            value: 'OTRO PADECIMIENTOS DE LA COLUMNA VERTEBRAL',
+            viewValue: 'OTRO PADECIMIENTOS DE LA COLUMNA VERTEBRAL',
+        },
+        {
+            value: 'OTRO TRASTORNO MÚSCULO ESQUELÉTICOS',
+            viewValue: 'OTRO TRASTORNO MÚSCULO ESQUELÉTICOS',
+        },
+    ],
+    name: 'ailment',
+};
+
+export const $cardiovascularSystemAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'PRESIÓN ARTERIAL ALTA',
+            viewValue: 'PRESIÓN ARTERIAL ALTA',
+        },
+        {
+            value: 'PROBLEMAS DEL CORAZÓN',
+            viewValue: 'PROBLEMAS DEL CORAZÓN',
+        },
+        {
+            value: 'SOPLOS',
+            viewValue: 'SOPLOS',
+        },
+        {
+            value: 'VALVULOPATÍAS',
+            viewValue: 'VALVULOPATÍAS',
+        },
+        {
+            value: 'FIEBRE REUMÁTICA',
+            viewValue: 'FIEBRE REUMÁTICA',
+        },
+        {
+            value: 'ANGINA',
+            viewValue: 'ANGINA',
+        },
+        {
+            value: 'INFARTO',
+            viewValue: 'INFARTO',
+        },
+        {
+            value: 'VARICES',
+            viewValue: 'VARICES',
+        },
+        {
+            value: 'FLEBITIS',
+            viewValue: 'FLEBITIS',
+        },
+        {
+            value: 'PATOLOGÍA CARDIACA',
+            viewValue: 'PATOLOGÍA CARDIACA',
+        },
+        {
+            value: 'OTROS PADECIMIENTO DEL SISTEMA CARDIOVASCULAR',
+            viewValue: 'OTROS PADECIMIENTO DEL SISTEMA CARDIOVASCULAR',
+        },
+
+    ],
+    name: 'ailment',
+};
+
+export const $nervousSystemAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'EPILEPSIA',
+            viewValue: 'EPILEPSIA',
+        },
+        {
+            value: 'DESMAYOS',
+            viewValue: 'DESMAYOS',
+        },
+        {
+            value: 'MAREOS',
+            viewValue: 'MAREOS',
+        },
+        {
+            value: 'CRISIS NERVIOSA',
+            viewValue: 'CRISIS NERVIOSA',
+        },
+        {
+            value: 'ANSIEDAD',
+            viewValue: 'ANSIEDAD',
+        },
+        {
+            value: 'DEPRESIÓN',
+            viewValue: 'DEPRESIÓN',
+        },
+        {
+            value: 'CONVULSIONES',
+            viewValue: 'CONVULSIONES',
+        },
+        {
+            value: 'OTROS PADECIMIENTOS DE CEREBRO O SISTEMA NERVIOSO',
+            viewValue: 'OTROS PADECIMIENTOS DE CEREBRO O SISTEMA NERVIOSO',
+        },
+    ],
+    name: 'ailment',
+};
+
+export const $visionHearingAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'VISIÓN DEFECTUOSA',
+            viewValue: 'VISIÓN DEFECTUOSA',
+        },
+        {
+            value: 'GLAUCOMA',
+            viewValue: 'GLAUCOMA',
+        },
+        {
+            value: 'CATARATAS',
+            viewValue: 'CATARATAS',
+        },
+        {
+            value: 'OTITIS',
+            viewValue: 'OTITIS',
+        },
+        {
+            value: 'LABERINTIS',
+            viewValue: 'LABERINTIS',
+        },
+        {
+            value: 'MALA AUDICIÓN',
+            viewValue: 'MALA AUDICIÓN',
+        },
+        {
+            value: 'OTROS PADECIMIENTOS DE LA VISTA Y/O OÍDO',
+            viewValue: 'OTROS PADECIMIENTOS DE LA VISTA Y/O OÍDO',
+        },
+    ],
+    name: 'ailment',
+};
+
+export const $respiratorySystemAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'TUBERCULOSIS',
+            viewValue: 'TUBERCULOSIS',
+        },
+        {
+            value: 'ENFISEMA',
+            viewValue: 'ENFISEMA',
+        },
+        {
+            value: 'BRONQUITIS',
+            viewValue: 'BRONQUITIS',
+        },
+        {
+            value: 'RINITIS',
+            viewValue: 'RINITIS',
+        },
+        {
+            value: 'SINUSITIS',
+            viewValue: 'SINUSITIS',
+        },
+        {
+            value: 'AMIGDALITIS',
+            viewValue: 'AMIGDALITIS',
+        },
+        {
+            value: 'ASMA',
+            viewValue: 'ASMA',
+        },
+        {
+            value: 'ALERGIAS',
+            viewValue: 'ALERGIAS',
+        },
+        {
+            value: 'OTROS PADECIMIENTOS DEL SISTEMA RESPIRATORIO',
+            viewValue: 'OTROS PADECIMIENTOS DEL SISTEMA RESPIRATORIO',
+        },
+
+    ],
+    name: 'ailment',
+};
+
+export const $digestiveSystemAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'HERNIA HIATAL',
+            viewValue: 'HERNIA HIATAL',
+        },
+        {
+            value: 'REFLUJO GASTROESOFÁGICO',
+            viewValue: 'REFLUJO GASTROESOFÁGICO',
+        },
+        {
+            value: 'GASTRITIS',
+            viewValue: 'GASTRITIS',
+        },
+        {
+            value: 'ÚLCERAS',
+            viewValue: 'ÚLCERAS',
+        },
+        {
+            value: 'COLITIS',
+            viewValue: 'COLITIS',
+        },
+        {
+            value: 'HEPATITIS',
+            viewValue: 'HEPATITIS',
+        },
+        {
+            value: 'DIVERTICULOSIS',
+            viewValue: 'DIVERTICULOSIS',
+        },
+        {
+            value: 'HEMORROIDES',
+            viewValue: 'HEMORROIDES',
+        },
+        {
+            value: 'PROBLEMA DE LOS INSTESTINOS',
+            viewValue: 'PROBLEMA DE LOS INSTESTINOS',
+        },
+        {
+            value: 'RECTO',
+            viewValue: 'RECTO',
+        },
+        {
+            value: 'HÍGADO',
+            viewValue: 'HÍGADO',
+        },
+        {
+            value: 'VESÍCULA BILIAR',
+            viewValue: 'VESÍCULA BILIAR',
+        },
+        {
+            value: 'PÁNCREAS',
+            viewValue: 'PÁNCREAS',
+        },
+        {
+            value: 'OTROS PADECIMIENTOS DEL SISTEMA DIGESTIVO',
+            viewValue: 'OTROS PADECIMIENTOS DEL SISTEMA DIGESTIVO',
+        },
+
+    ],
+    name: 'ailment',
+};
+
+export const $maleReproductiveOrgansAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'PADECIMIENTOS DE LA PRÓSTATA',
+            viewValue: 'PADECIMIENTOS DE LA PRÓSTATA',
+        },
+        {
+            value: 'TESTÍCULOS',
+            viewValue: 'TESTÍCULOS',
+        },
+        {
+            value: 'VARICOCELE',
+            viewValue: 'VARICOCELE',
+        },
+        {
+            value: 'OTROS PADECIMIENTOS DE LOS ÓRGANOS REPRODUCTIVOS MASCULINOS',
+            viewValue: 'OTROS PADECIMIENTOS DE LOS ÓRGANOS REPRODUCTIVOS MASCULINOS',
+        },
+    ],
+    name: 'ailment',
+};
+
+export const $bloodDisordersAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'ANEMIA',
+            viewValue: 'ANEMIA',
+        },
+        {
+            value: 'ANEMIA FALCIFORME',
+            viewValue: 'ANEMIA FALCIFORME',
+        },
+        {
+            value: 'HEMOFILIA',
+            viewValue: 'HEMOFILIA',
+        },
+        {
+            value: 'TRASTORNOS DE LA COAGULACIÓN',
+            viewValue: 'TRASTORNOS DE LA COAGULACIÓN',
+        },
+        {
+            value: 'OTROS PADECIMIENTOS SANGUINEOS',
+            viewValue: 'OTROS PADECIMIENTOS SANGUINEOS',
+        },
+
+    ],
+    name: 'ailment',
+};
+
+export const $endocrineDisordersAilment: FieldConfig =
+{
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'DIABETES',
+            viewValue: 'DIABETES',
+        },
+        {
+            value: 'COLESTEROL Y/O TRIGLICÉRIDOS ALTOS',
+            viewValue: 'COLESTEROL Y/O TRIGLICÉRIDOS ALTOS',
+        },
+        {
+            value: 'PADECIMIENTOS DE LA TIROIDES',
+            viewValue: 'PADECIMIENTOS DE LA TIROIDES',
+        },
+        {
+            value: 'GOTA O TRASTORNOS ENDÓCRINOS',
+            viewValue: 'GOTA O TRASTORNOS ENDÓCRINOS',
+        },
+    ],
+    name: 'ailment',
+};
+
+export const $alternateTreatmentAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'CÁNCER',
+            viewValue: 'CÁNCER',
+        },
+        {
+            value: 'TUMOR',
+            viewValue: 'TUMOR',
+        },
+        {
+            value: 'QUISTES',
+            viewValue: 'QUISTES',
+        },
+        {
+            value: 'CRECIMIENTO Y/O INFLAMACIÓN DE GANGLIOS LINFÁTICOS',
+            viewValue: 'CRECIMIENTO Y/O INFLAMACIÓN DE GANGLIOS LINFÁTICOS',
+        },
+        {
+            value: 'LEUCEMIA',
+            viewValue: 'LEUCEMIA',
+        },
+    ],
+    name: 'ailment',
+};
+
+export const $functionalLimitationAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'PRÓTESIS',
+            viewValue: 'PRÓTESIS',
+        },
+        {
+            value: 'IMPLANTES',
+            viewValue: 'IMPLANTES',
+        },
+        {
+            value: 'AMPUTACIÓN',
+            viewValue: 'AMPUTACIÓN',
+        },
+        {
+            value: 'SECUELAS DE ALGÚN TIPO DE LIMITACIÓN FUNCIONAL',
+            viewValue: 'SECUELAS DE ALGÚN TIPO DE LIMITACIÓN FUNCIONAL',
+        },
+
+    ],
+    name: 'ailment',
+};
+
+export const $deformityAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'DEFORMIDAD',
+            viewValue: 'DEFORMIDAD',
+        },
+        {
+            value: 'ENFERMEDAD',
+            viewValue: 'ENFERMEDAD',
+        },
+        {
+            value: 'DEFECTO CONGÉNITO',
+            viewValue: 'DEFECTO CONGÉNITO',
+        },
+        {
+            value: 'PÉRDIDA DEL USO DE LA AUDICIÓN',
+            viewValue: 'PÉRDIDA DEL USO DE LA AUDICIÓN',
+        },
+        {
+            value: 'PÉRDIDA DEL USO DE OJO(S)',
+            viewValue: 'PÉRDIDA DEL USO DE OJO(S)',
+        },
+        {
+            value: 'PÉRDIDA DE ALGÚN MIEMBRO ',
+            viewValue: 'PÉRDIDA DE ALGÚN MIEMBRO ',
+        },
+    ],
+    name: 'ailment',
+};
+
+export const $stdAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'ENFERMEDADES DE TRANSMISIÓN SEXUAL',
+            viewValue: 'ENFERMEDADES DE TRANSMISIÓN SEXUAL',
+        },
+        {
+            value: 'GONORREA',
+            viewValue: 'GONORREA',
+        },
+        {
+            value: 'SÍFILIS',
+            viewValue: 'SÍFILIS',
+        },
+        {
+            value: 'CLAMIDIA',
+            viewValue: 'CLAMIDIA',
+        },
+        {
+            value: 'INFECCIÓN POR VIRUS DEL PAPILOMA HUMANO',
+            viewValue: 'INFECCIÓN POR VIRUS DEL PAPILOMA HUMANO',
+        },
+        {
+            value: 'HERPES',
+            viewValue: 'HERPES',
+        },
+        {
+            value: 'OTRAS',
+            viewValue: 'OTRAS',
+        },
+
+    ],
+    name: 'ailment',
+};
+
+export const $urinarySystemAilment: FieldConfig =
+{
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'CÁLCULO RENALES',
+            viewValue: 'CÁLCULO RENALES',
+        },
+        {
+            value: 'NEFRITIS',
+            viewValue: 'NEFRITIS',
+        },
+        {
+            value: 'INFECCIONES URINARIAS',
+            viewValue: 'INFECCIONES URINARIAS',
+        },
+        {
+            value: 'SANGRE EN LA ORINA',
+            viewValue: 'SANGRE EN LA ORINA',
+        },
+        {
+            value: 'PADECIMIENTOS DEL RIÑÓN',
+            viewValue: 'PADECIMIENTOS DEL RIÑÓN',
+        },
+        {
+            value: 'OTROS PADECIMIENTOS DEL SISTEMA URINARIO',
+            viewValue: 'OTROS PADECIMIENTOS DEL SISTEMA URINARIO',
+        },
+    ],
+    name: 'ailment',
+};
+
+export const $cerebroVascularAilment: FieldConfig =
+{
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'EMBOLIA',
+            viewValue: 'EMBOLIA',
+        },
+        {
+            value: 'TROMBOSIS',
+            viewValue: 'TROMBOSIS',
+        },
+        {
+            value: 'MIGRAÑA',
+            viewValue: 'MIGRAÑA',
+        },
+        {
+            value: 'DOLORES DE CABEZA',
+            viewValue: 'DOLORES DE CABEZA',
+        },
+        {
+            value: 'OTROS PADECIMIENTOS CEREBRO VASCULARES',
+            viewValue: 'OTROS PADECIMIENTOS CEREBRO VASCULARES',
+        },
+    ],
+    name: 'ailment',
+};
+
+export const $reproductiveOrganDisordersAilment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'ABORTO',
+            viewValue: 'ABORTO',
+        },
+        {
+            value: 'DOLOR PÉLVICO',
+            viewValue: 'DOLOR PÉLVICO',
+        },
+        {
+            value: 'ENDOMETRIOSIS',
+            viewValue: 'ENDOMETRIOSIS',
+        },
+        {
+            value: 'TUMORES',
+            viewValue: 'TUMORES',
+        },
+        {
+            value: 'QUISTES',
+            viewValue: 'QUISTES',
+        },
+        {
+            value: 'CUALQUIER TIPO DE MASAS',
+            viewValue: 'CUALQUIER TIPO DE MASAS',
+        },
+        {
+            value: 'CERVICITIS',
+            viewValue: 'CERVICITIS',
+        },
+        {
+            value: 'ENFERMEDADES DE LOS SENOS',
+            viewValue: 'ENFERMEDADES DE LOS SENOS',
+        },
+        {
+            value: 'OVARIOS',
+            viewValue: 'OVARIOS',
+        },
+        {
+            value: 'TROMPAS',
+            viewValue: 'TROMPAS',
+        },
+        {
+            value: 'TRASTORNOS MENSTRUALES',
+            viewValue: 'TRASTORNOS MENSTRUALES',
+        },
+        {
+            value: 'HEMORRAGIAS MENSTRUALES',
+            viewValue: 'HEMORRAGIAS MENSTRUALES',
+        },
+        {
+            value: 'ALTERACIONES MENSTRUALES',
+            viewValue: 'ALTERACIONES MENSTRUALES',
+        },
+        {
+            value: 'DESORDENES DE LOS ÓRGANOS REPRODUCTIVOS',
+            viewValue: 'DESORDENES DE LOS ÓRGANOS REPRODUCTIVOS',
+        },
+    ],
+    name: 'ailment',
+};
+
+
+export const $Ailment: FieldConfig = {
+    label: 'Padecimiento',
+    options: [
+        {
+            value: 'EMBOLIA',
+            viewValue: 'EMBOLIA',
+        },
+
+    ],
+    name: 'ailment',
+};

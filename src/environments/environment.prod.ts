a8ac10432115bb9050e54bfce54b13cf14ed55d9
Juan Carlos Perez T <Juan Carlos Perez T@LAPTOP-BKITPDCM>
2020-08-09 21:59:55 -0400

    last changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,31 +7,29 @@ const keycloakConfig: KeycloakConfig = {
   clientId: 'cotizador',
 };
 
-
 export const environment = {
   production: true,
   instructivo: "http://wwsdevportalbackend.azurewebsites.net/INSTRUCTIVO_V2.pdf",
   instructivoMedical: "http://wwsdevportalbackend.azurewebsites.net/INSTRUCTIVO_WWM_V2.pdf",
   fileUrl: 'http://wwsdevportalbackend.azurewebsites.net',
   mailForHelp: "ayuda_wws@wws.com.do",
-  urlCotizadoresPdf: "https://cotizadores.wwseguros.com.do",
-  urlCotizadores: "https://cotizadores.wwseguros.com.do",
-  urlCotizadoresBoth: "https://cotizadores.wwseguros.com.do/?cia=",
-  urlCotizadoresVida: "https://cotizadores.wwseguros.com.do/vida",
-  urlCotizadoresSalud: "https://cotizadores.wwseguros.com.do/salud",
+  urlCotizadores: "http://portalwwg.eastus.cloudapp.azure.com:3000",
+  urlCotizadoresPdf: "http://portalwwg.eastus.cloudapp.azure.com:3031",
+  urlCotizadoresBoth: "http://portalwwg.eastus.cloudapp.azure.com:3000/?cia=",
+  urlCotizadoresVida: "http://portalwwg.eastus.cloudapp.azure.com:3000/vida",
+  urlCotizadoresSalud: "http://portalwwg.eastus.cloudapp.azure.com:3000/salud",
   mailForHelpPM: "ayuda_wwma@wwmedicalassurance.com",
-  baseUrl: 'https://portal.wwseguros.com.do/',
+  baseUrl: 'https://wwsdevfrontend-staging.azurewebsites.net/',
   keycloak: keycloakConfig,
   apiUrl: 'https://wwsdevportalbackend.azurewebsites.net',
   urlNotAccess: 'https://wwsdevportalbackend.azurewebsites.net/AccesoPortal/index/?location=',
   keycloakConfig
 };
 
-
 export const FormValidationsConstant = {
   maxMenAge: 50,
   MaleSexCode: 'M',
-  linkCotizadores: 'https://cotizadores.wwseguros.com.do/?cia=',
+  linkCotizadores: 'http://portalwwg.eastus.cloudapp.azure.com:3000/?cia=',
   titlesForMajorExpenses: [
     'Solicitante',
     'Persona políticamente expuesta',
@@ -49,3 +47,4 @@ export const FormValidationsConstant = {
     'Beneficiario(s) Contingente(s)',
     'Comentarios adicionales']
 };
+

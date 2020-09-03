b9d2b307610e09588b258d8d70348af5659010a2
Isai Vargas <isaidvargas@hotmail.com>
2020-09-03 12:19:22 -0400

    fix: permitir ñ en el input

--- src/app/shared/components/form-components/input/input.component.ts
+++ src/app/shared/components/form-components/input/input.component.ts
@@ -9,6 +9,7 @@ import { FormGroup } from '@angular/forms';
 })
 
 // tslint:disable: triple-equals
+// tslint:disable: max-line-length
 export class InputComponent implements OnInit, AfterViewChecked {
 	@Input() label: string;
 	@Input() name: string;
@@ -34,6 +35,6 @@ export class InputComponent implements OnInit, AfterViewChecked {
 	omitSpecialChar(event) {
 		let k;
 		k = event.charCode;
-		return ((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8 || k == 32 || (k >= 48 && k <= 57) || k == 44 || k == 46 || k == 64);
+		return ((k > 64 && k < 91) || (k > 96 && k < 123) || k === 8 || k === 32 || (k >= 48 && k <= 57) || k === 44 || k === 46 || k === 64 || k === 241 || k === 209);
 	}
 }

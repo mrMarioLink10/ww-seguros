09b13c0ff8eaeb9ee3f66f11138933707a40932a
Isai Vargas <isaidvargas@hotmail.com>
2021-06-18 19:25:57 -0400

    fix: pasar cambios a prod

--- src/app/modules/dashboard/requests/new-request/life/life.component.ts
+++ src/app/modules/dashboard/requests/new-request/life/life.component.ts
@@ -2011,7 +2011,7 @@ export class LifeComponent implements OnInit, DoCheck {
 
 
                 this.newRequest.get('releventPlanInformation').get('type').setValue(response.data.plan);
-                this.newRequest.get('releventPlanInformation').get('bonus').setValue(response.data.prima);
+                this.newRequest.get('releventPlanInformation').get('bonus').setValue(response.data.monto);
                 this.newRequest.get('releventPlanInformation').get('nicotineEstandar').setValue(response.data.nicotineStandar);
                 this.newRequest.get('releventPlanInformation').get('timeAmount').setValue(response.data.periodo_cobertura);
                 switch (response.data.sexo) {

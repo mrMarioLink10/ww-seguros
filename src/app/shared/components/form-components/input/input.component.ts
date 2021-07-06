b455ab3298462a87fbec61701af3afc7c8b8c0e9
zer0 <rhailinmade@hotmail.com>
2021-07-06 14:57:46 -0400

    feature: prod-branch-apr-2021, input component ahora acepta el caracter '_'

--- src/app/shared/components/form-components/input/input.component.ts
+++ src/app/shared/components/form-components/input/input.component.ts
@@ -37,7 +37,7 @@ export class InputComponent implements OnInit, AfterViewChecked {
 	omitSpecialChar(event) {
 		let k;
 		k = event.charCode;
-		return ((k > 64 && k < 91) || (k > 96 && k < 123) || k === 8 || k === 32 || (k >= 48 && k <= 57) || k === 44 || k === 46 || k === 64 || k === 241 || k === 209 || k === 32);
+		return ((k > 64 && k < 91) || (k > 96 && k < 123) || k === 8 || k === 32 || (k >= 48 && k <= 57) || k === 44 || k === 46 || k === 64 || k === 241 || k === 209 || k === 32 || k === 95);
 	}
 
 	goCap() {

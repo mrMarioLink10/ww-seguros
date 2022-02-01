8845f13bf6ae71c809e38679a3a8b43f1f3e3af6
zer0 <rhailinmade@hotmail.com>
2022-02-01 17:47:14 -0400

    feature: Branch=develop. Cambios/Avances= Arreglo de la funcionalidad de acceso a las varias vistas de la pagina en el archivo de wwm-access.guard.

--- src/app/core/guards/wwm-access.guard.ts
+++ src/app/core/guards/wwm-access.guard.ts
@@ -13,7 +13,26 @@ export class WwmAccessGuard implements CanActivate, CanActivateChild {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    if (this.userService.getRoleCotizador() === 'WMA') {
+      if ((this.userService.getRoles().includes('WWS') && this.userService.getRoles().includes('WMA'))) {
+        let country = localStorage.getItem('countryCode');
+        // console.log('countryCode es igual a ' + country);
+        let countryRole = '';
+        if (country == 'rd') {
+          countryRole = 'WWS';
+          // console.log('el country es igual a wws');
+        }
+        else if (country == 'pn') {
+          countryRole = 'WMA';
+          // console.log('el country es igual a wwm');
+        }
+
+        if (countryRole === 'WMA') {
+          console.log('No estás logueado');
+          this.router.navigate(['/']);
+          return false;
+        }
+      }
+      else if (this.userService.getRoleCotizador() === 'WMA') {
       console.log('No estás logueado');
       this.router.navigate(['/']);
       return false;
